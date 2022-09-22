2ff4517a7f55c2b48eda3c1d24d9a4d37be460f8
“zOUMINGXI” <17622333160@163.com>
2022-09-22 14:42:16 +0800

    20220922zmx

---
+++ 20220922zmx创建user-course关系/20220920READMEzmx.md
@@ -0,0 +1,11 @@
+Attention:目录格式不做具体要求，自行修改
+
+writer：zmx
+date：2022/09/20
+about:处理数据集，主要处理course，user，concept三个实体集合
+
+writer:zmx
+data:2022/09/22
+about:处理数据集，添加user-course关系，添加实体集合video
+
+
---
+++ 20220922zmx创建user-course关系/relation_user_course.py
@@ -0,0 +1,31 @@
+import json
+import numpy as np
+import pandas as pd
+import time
+import re
+import sys
+import codecs
+import pickle
+import three_entity_preprocess
+def read_json(file):
+    with open(file, "r", encoding='utf-8') as f:
+        data = f.readlines()
+        data = list(map(json.loads, data))
+    df = pd.DataFrame(data)
+    return df
+def get_user_course_relation():#index:id:course_order
+    data=read_json("../scripts/entities/user.json")
+    data=data[["id","course_order"]].reset_index()
+    return data
+# get_user_course_relation()
+
+
+
+
+
+
+
+
+
+
+
